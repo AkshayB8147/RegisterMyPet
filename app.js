752c0adfc08e3347160d26538fe08eb502bc1052
AkshayB8147 <tbhandge6@gmail.com>
2022-12-13 22:58:02 +0530

    petFormBS added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 const PORT=8147;
+
 app.get('/',(req,res)=>{
-res.send("Hello world");
+res.sendFile(path.join(__dirname+'/views/petFormBS.html'));
 });
 app.listen(PORT,()=>{
     console.log('App started on Port '+PORT);
